c8d256f3b20bdcfeca7b2d5405817c989dd0c8e8
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-11-08 14:24:54 -0800

    add Query and Mutation to the resolvers.js

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,4 +1,3 @@
-// import user model
 const { User } = require("../models");
 // import sign token function from auth
 const { signToken } = require("../utils/auth");
@@ -6,50 +5,50 @@ const { signToken } = require("../utils/auth");
 module.exports = {
   // get a single user by either their id or their username
   Query: {
-    getSingleUser: async(parent, args, context) => {
+    getSingleUser: async (parent, args, context) => {
       const foundUser = await User.findOne({
         username: args.username,
       });
-  
+
       if (!foundUser) {
         return console.error("Cannot find a user with this id!");
       }
-  
+
       return foundUser;
     },
   },
-  
+
   Mutation: {
-    async createUser(parent, args, context) {
+    createUser: async (parent, args, context) => {
       const user = await User.create(args);
-  
+
       if (!user) {
-        return console.error("Something is wrong!" );
+        return console.error("Something is wrong!");
       }
       const token = signToken(user);
       return { token, user };
     },
     // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
     // {body} is destructured req.body
-    async login(parent, args, context) {
+    login: async (parent, args, context) => {
       const user = await User.findOne({
         $or: [{ username: args.username }, { email: args.email }],
       });
       if (!user) {
         return console.error("Can't find this user");
       }
-  
+
       const correctPw = await user.isCorrectPassword(args.password);
-  
+
       if (!correctPw) {
         return console.error("Wrong password!");
       }
       const token = signToken(user);
-      return({ token, user });
+      return { token, user };
     },
     // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
     // user comes from `req.user` created in the auth middleware function
-    async saveBook(parent, args, context) {
+    saveBook: async (parent, args, context) => {
       try {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
@@ -63,7 +62,7 @@ module.exports = {
       }
     },
     // remove a book from `savedBooks`
-    async deleteBook(parent, args, context) {
+    deleteBook: async (parent, args, context) => {
       const updatedUser = await User.findOneAndUpdate(
         { _id: context.user._id },
         { $pull: { savedBooks: { bookId: args.bookId } } },
@@ -74,7 +73,5 @@ module.exports = {
       }
       return updatedUser;
     },
-
-  }
-  // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+  },
 };
