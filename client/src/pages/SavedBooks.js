03e13a6e752af3727b9fe9e140f801e6f299cfe2
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-12-06 15:18:10 -0800

    change deployed address

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import React from "react";
+// import { Navigate } from "react-router-dom";
+import { redirect } from "react-router-dom";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
 
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutation';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutation";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
   const [deleteBook] = useMutation(REMOVE_BOOK);
   const userData = data?.me || {};
-  
-  if(!userData?.username) {
+
+  if (!userData?.username) {
     return (
       <h4>
-        You need to be looged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be looged in to see this page. Use the navigation links
+        above to sign up or log in!
       </h4>
     );
   }
@@ -23,28 +32,31 @@ const SavedBooks = () => {
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if(!token) {
+    if (!token) {
       return false;
     }
 
     try {
       await deleteBook({
-        variables: {bookId: bookId},
+        variables: { bookId: bookId },
       });
       removeBookId(bookId);
       // window.location.reload();
+
+      // return <Navigate to="/" replace={true} />;
+      return redirect("/");
     } catch (err) {
       console.error(err);
-    };
+    }
 
-    if(loading) {
-      return <h2>LOADING...</h2>
+    if (loading) {
+      return <h2>LOADING...</h2>;
     }
   };
-  
+
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -52,19 +64,30 @@ const SavedBooks = () => {
       <Container>
         <h2>
           {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
         <CardColumns>
           {userData.savedBooks.map((book) => {
             return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              <Card key={book.bookId} border="dark">
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
+                  <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
                     Delete this Book!
                   </Button>
                 </Card.Body>
@@ -78,4 +101,3 @@ const SavedBooks = () => {
 };
 
 export default SavedBooks;
-
